18ce246ebc3f8e09af9348e130d08121a6c9543f
Adam Sickels <47608860+adamsickels00@users.noreply.github.com>
2020-11-11 16:08:02 -0500

    full test set for rails

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -2,7 +2,27 @@ require 'rails_helper'
 
 describe 'The application' do
     it 'is running' do
-        get('/')
-        expect(response.body).to_not be(nil)
+        get('/students')
+        expect(response.status).to eq(200)
+        expect(response).to render_template('students/index')
+    end
+
+    it 'can add students' do
+        get('/students/new')
+        expect(response).to render_template(:new)
+
+        post "/students", :params => { :student => {:name => "Paul"} }
+
+        expect(response).to redirect_to(assigns(:student))
+        follow_redirect!
+
+        expect(response).to render_template(:show)
+        expect(response.body).to include("Student was successfully created.")
+        expect(Student.all.length).to eq(1)
+    end
+
+    it 'can return all the students' do
+        get('/students')
+        expect(assigns(:students)).to eq(Student.all)
     end
 end
